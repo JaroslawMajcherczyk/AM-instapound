73d117200b1f2813f7fbbdbab93ddfd5e926221f
Eryk Ngo Ngoc <eryk@ngo-ngoc.dev>
2021-12-27 20:30:23 +0100

    edit view ready, needs implementing image picking and PUT api call

--- src/screens/MyProfileEditScreen.js
+++ src/screens/MyProfileEditScreen.js
@@ -1,13 +1,52 @@
-import React from 'react'
-import { Text, SafeAreaView} from 'react-native'
-import GlobalStyles from "../utils/GlobalStyles";
+import React, {useRef, useState} from 'react'
+import {Image, StyleSheet, Platform, KeyboardAvoidingView} from 'react-native'
+import {Button, TextArea, View} from "native-base";
 
 const MyProfileEditScreen = () => {
+    const [picture, setPicture] = useState(null);
+    const [bio, setBio] = useState(null)
+
+    const source = picture !== null ? {uri: picture} : require('../../assets/images/user-placeholder.png');
+
+
+
+    const submit = () => {
+        console.log(picture)
+        console.log(bio)
+    }
+
     return (
-        <SafeAreaView style={GlobalStyles.droidSafeArea}>
-            <Text>Here you will edit your profile!</Text>
-        </SafeAreaView>
+            <KeyboardAvoidingView
+                style={{flex: 1, alignItems: "center"}}
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+                <View style={{alignItems: "center", marginVertical: 15}}>
+                    <Image source={source} style={styles.picture}/>
+                    <Button.Group size="sm" style={{marginTop: 5}}>
+                        <Button>Take a picture</Button>
+                        <Button>Select picture</Button>
+                    </Button.Group>
+                </View>
+                <TextArea onChangeText={text => setBio(text)} h={220} placeholder={"Set your bio here!"} w={350}/>
+                <Button width={350} style={styles.bottomButton} onPress={submit}>Save</Button>
+            </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    picture: {
+        width: 300,
+        height: 300,
+        borderWidth: 3,
+        borderColor: '#444',
+    },
+    center: {
+        alignItems: 'center'
+    },
+    bottomButton: {
+        position: 'relative',
+        top: 25
+    },
+});
 
 export default MyProfileEditScreen
